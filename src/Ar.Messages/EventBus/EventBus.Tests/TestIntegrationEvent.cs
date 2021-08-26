9fc476c15fdc5dc5b975580a1ffa15c0fa1dda18
Evgeny Volynsky <evgeny.volynsky@tum.de>
2021-08-26 14:33:34 +0200

    * [EventBus] add event bus abstraction library, rabbitmq project, event bus test project

---
+++ src/Ar.Messages/EventBus/EventBus.Tests/TestIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿using Ar.Messages.EventBus.EventBus.Events;
+
+namespace EventBus.Tests
+{
+    public record TestIntegrationEvent : IntegrationEvent
+    {
+    }
+}
---
+++ src/Ar.Messages/EventBus/EventBus.Tests/TestIntegrationOtherEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Ar.Messages.EventBus.EventBus.Abstractions;
+using System.Threading.Tasks;
+
+namespace EventBus.Tests
+{
+    public class TestIntegrationOtherEventHandler : IIntegrationEventHandler<TestIntegrationEvent>
+    {
+        public bool Handled { get; private set; }
+
+        public TestIntegrationOtherEventHandler()
+        {
+            Handled = false;
+        }
+
+        public async Task Handle(TestIntegrationEvent @event)
+        {
+            Handled = true;
+        }
+    }
+}
